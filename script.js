02c0c35a242d27d47b40caabbe0a4b67df8d28cf
Krasimir Ivanov <k.ivanov29@yahoo.com>
2022-08-24 18:00:49 +0100

    Set practice

--- script.js
+++ script.js
@@ -1,144 +1,83 @@
 'use strict';
-/* 
-Let's continue with our football betting app!
-
-1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
-2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
-3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
-      Odd of victory Bayern Munich: 1.33
-      Odd of draw: 3.25
-      Odd of victory Borrussia Dortmund: 6.5
-Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
-
-BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
-      {
-        Gnarby: 1,
-        Hummels: 1,
-        Lewandowski: 2
-      }
-
-GOOD LUCK 😀
-*/
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
-//1
-for (const [score, player] of game.scored.entries()) {
-  console.log(`Goal ${score + 1}: ${player}`);
-}
-//2
-let totalOdds = 0;
-for (const odds of Object.values(game.odds)) {
-  totalOdds += odds;
-}
-totalOdds /= Object.keys(game.odds).length;
-console.log(totalOdds.toFixed(2));
-//3
-for (let key of Object.keys(game.odds)) {
-  console.log(
-    `Odd of ${game?.[key] ? `victory ${game[key]}` : `draw`} : ${
-      game.odds[key]
-    }`
-  );
-}
-//Bonus
-const scorers = {};
-for (const player of game.scored) {
-  if (scorers.hasOwnProperty(player)) {
-    let newScore = scorers[player] + 1;
-    scorers[player] = newScore;
-  } else {
-    scorers[player] = 1;
-  }
-}
-console.log(scorers);
 
 // // Data needed for a later exercise
 // // const flights =
 // //   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // // Data needed for first part of the section
-// const weekDays = [`mon`, `tue`, `wed`, `thu`, `fri`, `sat`, `sun`];
-// const openingHours = {
-//   [weekDays[3]]: {
-//     open: 12,
-//     close: 22,
-//   },
-//   [weekDays[4]]: {
-//     open: 11,
-//     close: 23,
-//   },
-//   [weekDays[5]]: {
-//     open: 0, // Open 24 hours
-//     close: 24,
-//   },
-// };
+const weekDays = [`mon`, `tue`, `wed`, `thu`, `fri`, `sat`, `sun`];
+const openingHours = {
+  [weekDays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekDays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekDays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
 
-// const restaurant = {
-//   name: 'Classico Italiano',
-//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
-//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-//   // Es6 enhanced object literals
-//   openingHours,
-//   order(starterIndex, mainIndex) {
-//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-//   },
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  // Es6 enhanced object literals
+  openingHours,
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 
-//   orderDelivery({ starterIndex = 1, mainIndex = 0, time = `20:00`, address }) {
-//     console.log(
-//       `Order recived! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}.`
-//     );
-//   },
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = `20:00`, address }) {
+    console.log(
+      `Order recived! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}.`
+    );
+  },
 
-//   orderPasta(ing1, ing2, ing3) {
-//     console.log(
-//       `Here is your delicius pasta with ${ing1},${ing2} and ${ing3}.`
-//     );
-//   },
-//   orderPizza(mainIngredient, ...otherIngredient) {
-//     console.log(mainIngredient);
-//     console.log(otherIngredient);
-//   },
-// };
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicius pasta with ${ing1},${ing2} and ${ing3}.`
+    );
+  },
+  orderPizza(mainIngredient, ...otherIngredient) {
+    console.log(mainIngredient);
+    console.log(otherIngredient);
+  },
+};
+const ordersSet = new Set([
+  `Pasta`,
+  `Pizza`,
+  `Pizza`,
+  `Risotto`,
+  `Pasta`,
+  `Pizza`,
+]);
+console.log(ordersSet);
+
+console.log(new Set(`Jonas`));
+console.log(ordersSet.size);
+console.log(ordersSet.has(`Pizza`));
+console.log(ordersSet.has(`Bread`));
+ordersSet.add(`Garlic Bread`);
+ordersSet.add(`Garlic Bread`);
+ordersSet.delete(`Risotto`);
+//ordersSet.clear();
+console.log(ordersSet);
+for (const order of ordersSet) {
+  console.log(order);
+}
+// Example
+const staff = [`Waiter`, `Chef`, `Waiter`, `Manager`, `Chef`, `Waiter`];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+console.log(
+  new Set([`Waiter`, `Chef`, `Waiter`, `Manager`, `Chef`, `Waiter`]).size
+);
 
 // if (restaurant.openingHours && restaurant.openingHours.mon)
 //   console.log(restaurant.openingHours.mon.open);
