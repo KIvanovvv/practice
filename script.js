c08be80db4153f271f94d7960ba1a77ef2fcebaf
Krasimir Ivanov <k.ivanov29@yahoo.com>
2022-08-24 18:18:47 +0100

    Maps practice

--- script.js
+++ script.js
@@ -57,27 +57,57 @@ const ordersSet = new Set([
   `Pasta`,
   `Pizza`,
 ]);
-console.log(ordersSet);
 
-console.log(new Set(`Jonas`));
-console.log(ordersSet.size);
-console.log(ordersSet.has(`Pizza`));
-console.log(ordersSet.has(`Bread`));
-ordersSet.add(`Garlic Bread`);
-ordersSet.add(`Garlic Bread`);
-ordersSet.delete(`Risotto`);
-//ordersSet.clear();
-console.log(ordersSet);
-for (const order of ordersSet) {
-  console.log(order);
-}
-// Example
-const staff = [`Waiter`, `Chef`, `Waiter`, `Manager`, `Chef`, `Waiter`];
-const staffUnique = [...new Set(staff)];
-console.log(staffUnique);
-console.log(
-  new Set([`Waiter`, `Chef`, `Waiter`, `Manager`, `Chef`, `Waiter`]).size
-);
+const rest = new Map();
+rest.set(`name`, `Classico Ialiano`);
+rest.set(1, `Firenze,Italy`);
+rest.set(2, `Lisbon,Portugal`);
+//console.log(rest);
+rest
+  .set(`categories`, ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set(`open`, 11)
+  .set(`close`, 23)
+  .set(true, `We are open`)
+  .set(false, `We are close`);
+
+// console.log(rest.get(`name`));
+// console.log(rest.get(true));
+
+const time = 21;
+//.log(rest.get(time > rest.get(`open`) && time < rest.get(`close`)));
+
+//console.log(rest.has(`categories`));
+rest.delete(2);
+//rest.clear();
+const arr = [1, 2];
+rest.set(arr, `test`);
+rest.set(document.querySelector(`h1`), `heading`);
+console.log(rest);
+console.log(rest.get(arr));
+
+////////////////////////////////
+//Sets
+// console.log(ordersSet);
+
+// console.log(new Set(`Jonas`));
+// console.log(ordersSet.size);
+// console.log(ordersSet.has(`Pizza`));
+// console.log(ordersSet.has(`Bread`));
+// ordersSet.add(`Garlic Bread`);
+// ordersSet.add(`Garlic Bread`);
+// ordersSet.delete(`Risotto`);
+// //ordersSet.clear();
+// console.log(ordersSet);
+// for (const order of ordersSet) {
+//   console.log(order);
+// }
+// // Example
+// const staff = [`Waiter`, `Chef`, `Waiter`, `Manager`, `Chef`, `Waiter`];
+// const staffUnique = [...new Set(staff)];
+// console.log(staffUnique);
+// console.log(
+//   new Set([`Waiter`, `Chef`, `Waiter`, `Manager`, `Chef`, `Waiter`]).size
+// );
 
 // if (restaurant.openingHours && restaurant.openingHours.mon)
 //   console.log(restaurant.openingHours.mon.open);
